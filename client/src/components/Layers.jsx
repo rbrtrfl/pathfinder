5d25ecd9ffb2229314bae4c79758730ef7e7704f
Rafael Falk <rbrtrflflk@gmail.com>
2022-03-03 17:13:21 +0100

    feature: display map

---
+++ client/src/components/Layers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Layers() {
+  return (
+    <div>Layers</div>
+  )
+}
+
+export default Layers
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+import Layers from './Layers'
 
 function Map() {
   return (
-    <div>Map</div>
+    <div className='map'>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+        crossOrigin=""
+      />
+      <script
+        src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossOrigin=""
+      />
+      <MapContainer center={[46.8403752, 9.0290986]} zoom={14}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png"
+        />
+        <Layers />
+      </MapContainer>
+    </div>
   )
 }
 
