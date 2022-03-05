07c3465e52279a29858dd13d016d98044fcca380
Rafael Falk <rbrtrflflk@gmail.com>
2022-03-05 09:52:14 +0100

    feature: file upload

--- client/src/components/FileUploadForm.jsx
+++ client/src/components/FileUploadForm.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { postTrack } from '../services/ApiService';
 
 function FileUploadForm() {
 
-  function clickHandler() {
-    
+  function clickHandler(event) {
+    event.preventDefault();
+    const file = document.getElementById('input').files[0];
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.addEventListener('loadend', (data) => {
+      postTrack(data.currentTarget.result);
+    })
   }
 
   return (
     <div>FileUploadForm
-      <form onClick={clickHandler()}>
-        <input type="file"></input>
-        <button>UPLOAD</button>
+      <form onSubmit={clickHandler}>
+        <input type="file" id="input"></input>
+        <button type="submit">UPLOAD</button>
       </form>
     </div>
   )
--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,19 +1,28 @@
+import GpxParser from 'gpxparser'
+
 const baseurl = 'http://127.0.0.1:4000'
 
 export function getAll() {
-return fetch(`${baseurl}/tracks`)
-.then((data) => data.json())
-.catch((error) => console.error(error));
+  return fetch(`${baseurl}/tracks`)
+    .then((data) => data.json())
+    .catch((error) => console.error(error));
 }
 
-export function postTrack(gpx) {
-return fetch(`${baseurl}/tracks`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: gpx
-})
-.then((data) => data.json())
-.catch((error) => console.error(error));
+export function postTrack(file) {
+  // const gpx = new GpxParser();
+  // gpx.parse(file);
+  // console.log(gpx);
+  const data = {
+    track: file
+  }
+
+  return fetch(`${baseurl}/tracks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then((data) => data.json())
+    .catch((error) => console.error(error));
 }
